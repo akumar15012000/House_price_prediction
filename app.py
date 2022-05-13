09a643187dbedca40cd9c38ed9894be6c5e4889e
Amit Kumar <akumar15012000@gmail.com>
2022-05-13 13:28:23 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,47 @@
+
+
+
+
+from unicodedata import name
+from flask import Flask,redirect,url_for,render_template,request
+import util
+
+# jinja2 technique
+'''
+{{ value }}expression 
+{%  %}statment
+{#.....#}comment
+
+'''
+
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def fun():
+    location=util.load_location()
+    
+    return render_template('index.html',loc=location,pred=0)
+
+
+
+
+@app.route('/submit',methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        res=dict(request.form)
+        loc=res['location']
+        bhk=int(res['bhk'].split(' ')[0])
+        bath=int(res['bath'].split(' ')[0])
+        area=float(res['area'])
+        predicted_val=util.predict_price(loc,bhk,area,bath)
+        locations=util.load_location()
+        return render_template('index.html',loc=locations,pred=predicted_val)
+
+
+
+
+
+if __name__=='__main__':
+    app.run()
---
+++ util.py
@@ -0,0 +1,57 @@
+import pickle
+import json
+from pyexpat import model
+import numpy as np
+
+locations=None
+model=None
+columns=None
+
+def load_location():
+
+    with open("model\column.json",'r') as f:
+        global locations
+        global columns
+        columns=json.load(f)['data_columns']
+        locations=columns[3:]
+
+        return locations
+
+
+def load_model():
+
+    with open('model\\bangalorea_house_prices.pickle','rb') as f:
+        global model
+        model=pickle.load(f)
+
+
+
+        
+def predict_price(loc,bhk,area,bath):
+    
+ 
+
+    try:
+        ind=columns.index(loc.lower())
+    except:
+        ind=-1
+    
+    load_location()
+    load_model()
+    x=np.zeros(len(columns))
+
+    x[0]=bhk
+    x[1]=area
+    x[2]=bath
+
+    if ind>=0:
+        x[ind]=1
+    
+    return model.predict([x])[0].round(3)
+
+
+if __name__=='__main__':
+    load_location()
+    load_model()
+    print(predict_price('1st Phase JP Nagar',2,1000,2))
+    
